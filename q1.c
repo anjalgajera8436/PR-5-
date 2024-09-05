004d05750e4a93f10065913f658230b43143d7d9
anjalgajera8436 <8436anjalgajera@gmail.com>
2024-09-05 02:30:24 -0400

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n,i;
+	
+	printf("enter array size"); 
+	scanf("%d",&n); 
+	
+	int arr[a]
+	printf("enter array elements;\n");
+	scanf("%d",&arr[i]);
+	
+	for( i=0; i<n; i++)
+	{
+		
+	}
+}
